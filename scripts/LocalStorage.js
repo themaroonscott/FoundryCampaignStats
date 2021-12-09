cf3778166db25ce1a4a077aba87b45d0ac3f8184
Jacob Buchanan <jbuchanan1304@gmail.com>
2021-12-09 16:36:44 -0700

    Add LocalStorage.js

---
+++ scripts/LocalStorage.js
@@ -0,0 +1,13 @@
+import { STORAGE_NAME } from "./Settings";
+
+export function GetItemFromLocalStorage(item) {
+    return JSON.parse(window.localStorage.getItem(item));
+}
+
+export function SaveToLocalStorageStat(statType, data) {
+    window.localStorage.setItem(statType, JSON.stringify(data));
+}
+
+export function TruncateLocalStorage() {
+    window.localStorage.removeItem(STORAGE_NAME);
+}
